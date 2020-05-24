7376c92147023b5a5a78183228f8a539e5d31852
sumit <sumitln2000@gmail.com>
2020-05-24 11:41:26 +0530

    some changes

--- app.js
+++ app.js
@@ -58,6 +58,6 @@ app.get("/post/:postName",function(req,res){
 
 
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT, function() {
   console.log("Server started on port 3000");
 });
